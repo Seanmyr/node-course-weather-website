5f25378a1d22a3d001396e82cb08d7a68a0175a3
Sean Meyer <seanmyr@gmail.com>
2020-08-20 14:31:58 -0500

    Added wind speed and wind direction to the forecast data.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (lat, long, callback) => {
 		} else if(body.error) {
 			callback('Unable to find location.');
 		} else {
-			callback(undefined, body.current.weather_descriptions[0] + ". It is currently "+body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.");
+			let forecast = body.current.weather_descriptions[0] + ". It is currently "+body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out.";
+			forecast += " The current wind speed is " + body.current.wind_speed + " and the wind direction is " + body.current.wind_dir;
+			callback(undefined, forecast);
 		}
 	});
 }
